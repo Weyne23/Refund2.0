a263c156124cce45dc69ea73de56761b2dd2b2b5
Weyne <augustoweyne12@gmail.com>
2025-08-28 20:46:14 -0300

    feature: criado link para singup

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -9,7 +9,7 @@ export function Button({isLoading, children, type="button", ...rest} : Props) {
             disabled={isLoading} 
             {...rest} 
             className="flex items-center justify-center bg-green-100 rounded-lg 
-            text-white cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 cursor-progress h-12"
+            text-white cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-progress h-12"
         >
             {children}
         </button>
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,13 +1,23 @@
 import { Input } from "../Components/Input"
 import { Button } from "../Components/Button"
+import { useState } from "react";
 
 export function SignIn() {
-    return ( 
-        <form action="" className="w-full flex flex-col gap-4">
-            <Input required legend="E-mail" type="email" placeholder="seu@email.com"/>
-            <Input required legend="Senha" type="password" placeholder="123456"/>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
-            <Button>Entrar</Button>
+    function onSubmit(e:React.FormEvent) {
+        e.preventDefault();
+    }
+
+    return ( 
+        <form onSubmit={onSubmit} action="" className="w-full flex flex-col gap-4">
+            <Input required legend="E-mail" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="seu@email.com"/>
+            <Input required legend="Senha" value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="123456"/>
+
+            <Button type="submit" isLoading={isLoading}>Entrar</Button>
+            <a href="/signup" className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear">Criar conta</a>
         </form>
      )
 }
