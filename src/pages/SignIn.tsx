5f6b3ea77c6e6034d2e3f1bf32bd1999c88b8141
Weyne <augustoweyne12@gmail.com>
2025-09-12 11:33:20 -0300

    improv: utilizado Zod para validar login e efetuando o login em si

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -3,7 +3,15 @@ import { Button } from "../Components/Button"
 
 //import { useState } from "react";
 import { useActionState } from "react";
-import { promise } from "zod";
+import { z, ZodError } from "zod";
+import { AxiosError } from "axios";
+
+import { api } from "../services/api";
+
+const signInScheme = z.object({
+    email: z.string().email({ message: "E-mail inválido!"}),
+    password: z.string().trim().min(1, {message: "Informe a senha!"})
+})
 
 export function SignIn() {
     //Foram removidos para usar o action que não precisa utilizar estados
@@ -14,25 +22,40 @@ export function SignIn() {
     //Para o array que o "useActionState" retorna a primeira posição é o estado atual do formulário,  a segunda é qual action será executada e a terceira é se a requisição ainda está pendente ou não.
 
     //Já os parâmetros do "useActionState" são a função que iremos executar e conteúdo padrão que o "useActionState" tera respectivamente.
-    const [state, formAction, isLoading] = useActionState(signIn, {
-        email: "",
-        password: ""
-    });
+    const [state, formAction, isLoading] = useActionState(signIn, null);
 
     //prevState é o estado anterior do formulário e o formDate é ds dados do formulário
-    async function signIn(prevState: any, formData : FormData) {
-        const email = formData.get("email");
-        const password = formData.get("password");
+    async function signIn(_: any, formData : FormData) {
+        try {
+            const data = signInScheme.parse({
+                email: formData.get("email"),
+                password : formData.get("password")
+            })
 
-        return { email, password }
+            const response = await api.post("/sessions", data)
+
+        } 
+        catch (error) {
+            console.log(error)
+
+            if(error instanceof ZodError)
+                return { message: error.issues[0].message }
+
+            if(error instanceof AxiosError)
+                return { message: error.response?.data.message }
+
+            return { message: "Erro ao efetuar login!" }
+        }
     }
 
     return ( 
         //Esse action é uma novidade do react 19
         <form action={formAction} className="w-full flex flex-col gap-4">
-            <Input name="email" required legend="E-mail" type="email" placeholder="seu@email.com" defaultValue={String(state?.email)}/>
-
-            <Input name="password" required legend="Senha" type="password" placeholder="123456" defaultValue={String(state?.password)}/>
+            <Input name="email" required legend="E-mail" type="email" placeholder="seu@email.com"/>
+            
+            <Input name="password" required legend="Senha" type="password" placeholder="123456"/>
+            
+            <p className="text-sm text-red-600 text-center my-4 font-medium">{state?.message}</p>
 
             <Button type="submit" isLoading={isLoading}>Entrar</Button>
             <a href="/signup" className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear">Criar conta</a>
